aa3fcf4d3e0a72f7e7208952320b196c627bcf6d
kevin <kev.church@gmail.com>
2018-02-14 18:14:05 +0000

    Enable scrolling on x axis

--- background.h
+++ background.h
@@ -6,6 +6,7 @@
 #define UNTITLED_BACKGROUND_H
 
 #include <SFML/Graphics.hpp>
+enum ScrollAxis { X_AXIS, Y_AXIS };
 
 class Background {
 
@@ -14,63 +15,118 @@ private:
     sf::RectangleShape first;
     sf::RectangleShape second;
     sf::Vector2u textureSize;
-    sf::Vector2f scrollSpeed;
+    float scrollSpeed;
+	ScrollAxis scrollAxis;
 
 public:
-    Background(std::string texturePath, sf::Vector2f startPosition, sf::Vector2f scrollSpeed) {
+    Background(std::string texturePath, sf::Vector2f startPosition, float scrollSpeed, ScrollAxis scrollAxis) {
         backgroundTexture.loadFromFile(texturePath);
         this->textureSize = backgroundTexture.getSize();
         this->scrollSpeed = scrollSpeed;
+		this->scrollAxis = scrollAxis;
 
         first = sf::RectangleShape(sf::Vector2f(textureSize));
         first.setTexture(&backgroundTexture);
-        first.setPosition(startPosition.x - (textureSize.x / 2), 0.0f);
 
         second = sf::RectangleShape(sf::Vector2f(textureSize));
         second.setTexture(&backgroundTexture);
-        second.setPosition(startPosition.x - (textureSize.x / 2), startPosition.y -(float)textureSize.y);
+
+		if (scrollAxis == X_AXIS) {
+			first.setPosition(0.0f, startPosition.y - (textureSize.y / 2));
+			second.setPosition(0.0f - (float)textureSize.x, startPosition.y - (textureSize.y / 2));
+		}
+		else {
+			first.setPosition(startPosition.x - (textureSize.x / 2), 0.0f);
+			second.setPosition(startPosition.x - (textureSize.x / 2), startPosition.y -(float)textureSize.y);
+		}
     }
 
     ~Background() {}
 
     void update (float delta) {
-        sf::Vector2f firstPos = first.getPosition() + (scrollSpeed * delta);
-        sf::Vector2f secondPos = second.getPosition() + (scrollSpeed * delta);
+        float firstPos;
+        float secondPos;
 
-        if (scrollSpeed.y > 0) {
-            if (firstPos.y >= textureSize.y) {
-                firstPos.y = secondPos.y - textureSize.y;
-            }
-            else if (secondPos.y >= textureSize.y) {
-                secondPos.y = firstPos.y - textureSize.y;
-            }
+		if (this->scrollAxis == X_AXIS) {
+			firstPos = first.getPosition().x + (scrollSpeed * delta);
+			secondPos = second.getPosition().x + (scrollSpeed * delta);
+			float y = first.getPosition().y;
 
-            if (firstPos.y > secondPos.y) {
-                first.setPosition(firstPos);
-                second.setPosition(secondPos.x, firstPos.y - textureSize.y);
-            }
-            else {
-                second.setPosition(secondPos);
-                first.setPosition(firstPos.x, secondPos.y - textureSize.y);
-            }
-        }
-        else if (scrollSpeed.y < 0) {
-            if (firstPos.y <= 0.0f - textureSize.y) {
-                firstPos.y = secondPos.y + textureSize.y;
-            }
-            else if (secondPos.y <= 0.0f - textureSize.y) {
-                secondPos.y = firstPos.y + textureSize.y;
-            }
+			if (scrollSpeed > 0) {
+				if (firstPos >= textureSize.x) {
+					firstPos = secondPos - textureSize.x;
+				}
+				else if (secondPos >= textureSize.x) {
+					secondPos = firstPos - textureSize.x;
+				}
 
-            if (firstPos.y < secondPos.y) {
-                first.setPosition(firstPos);
-                second.setPosition(secondPos.x, firstPos.y + textureSize.y);
-            }
-            else {
-                second.setPosition(secondPos);
-                first.setPosition(firstPos.x, secondPos.y + textureSize.y);
-            }
-        }
+				if (firstPos > secondPos) {
+					first.setPosition(firstPos, y);
+					second.setPosition(firstPos - textureSize.x, y);
+				}
+				else {
+					second.setPosition(secondPos, y);
+					first.setPosition(secondPos - textureSize.x, y);
+				}
+			}
+			else if (scrollSpeed < 0) {
+				if (firstPos <= 0.0f - textureSize.x) {
+					firstPos = secondPos + textureSize.x;
+				}
+				else if (secondPos <= 0.0f - textureSize.x) {
+					secondPos = firstPos + textureSize.x;
+				}
+
+				if (firstPos < secondPos) {
+					first.setPosition(firstPos, y);
+					second.setPosition(firstPos + textureSize.x, y);
+				}
+				else {
+					second.setPosition(secondPos, y);
+					first.setPosition(secondPos + textureSize.x, y);
+				}
+			}
+		}
+		else {
+			firstPos = first.getPosition().y + (scrollSpeed * delta);
+			secondPos = second.getPosition().y + (scrollSpeed * delta);
+			float x = first.getPosition().x;
+
+			if (scrollSpeed > 0) {
+				if (firstPos >= textureSize.y) {
+					firstPos = secondPos - textureSize.y;
+				}
+				else if (secondPos >= textureSize.y) {
+					secondPos = firstPos - textureSize.y;
+				}
+
+				if (firstPos > secondPos) {
+					first.setPosition(x, firstPos);
+					second.setPosition(x, firstPos - textureSize.y);
+				}
+				else {
+					second.setPosition(x, secondPos);
+					first.setPosition(x, secondPos - textureSize.y);
+				}
+			}
+			else if (scrollSpeed < 0) {
+				if (firstPos <= 0.0f - textureSize.y) {
+					firstPos = secondPos + textureSize.y;
+				}
+				else if (secondPos <= 0.0f - textureSize.y) {
+					secondPos = firstPos + textureSize.y;
+				}
+
+				if (firstPos < secondPos) {
+					first.setPosition(x, firstPos);
+					second.setPosition(x, firstPos + textureSize.y);
+				}
+				else {
+					second.setPosition(x, secondPos);
+					first.setPosition(x, secondPos + textureSize.y);
+				}
+			}
+		}
     }
 
     sf::RectangleShape getFirst() {
