5959e8de763cad993af7dbb2af6d09cded2e1637
kevin <kev.church@gmail.com>
2019-08-18 12:43:45 +0100

    Add missing libs to build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gcc main.cpp background.cpp following-background.cpp system.cpp world.cpp -o libs -lsfml-graphics -lsfml-window -lsfml-system -lstdc++
+gcc main.cpp background.cpp following-background.cpp system.cpp world.cpp -o libs -lm -ljsoncpp -lsfml-graphics -lsfml-window -lsfml-system -lstdc++
 ./libs
